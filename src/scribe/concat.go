36d040c3cbc1a0b35afd00838aca7586685f991c
Aaron Meihm <ameihm@mozilla.com>
2015-08-03 12:23:10 -0500

    key concat on identifier

--- src/scribe/concat.go
+++ src/scribe/concat.go
@@ -7,23 +7,33 @@
 
 package scribe
 
+// This function does criteria concatenation based on identifier. Where
+// criteria is found with a matching identifier, the results are
+// concatenated together. Since the slice is ordered based on expression
+// groups there should not be any ordering issues.
 func criteriaConcat(in []evaluationCriteria, concat string) []evaluationCriteria {
 	ret := make([]evaluationCriteria, 0)
 	if len(in) == 0 {
 		return ret
 	}
-	debugPrint("criteriaConcat(): applying concat with \"%v\"\n", concat)
-	n := evaluationCriteria{}
+	debugPrint("criteriaConcat(): applying concat with \"%v\" on identifier\n", concat)
+	retmap := make(map[string]evaluationCriteria, 0)
 	for _, x := range in {
-		if len(n.testValue) == 0 {
-			n.identifier = "concat:" + x.identifier
-			n.testValue = x.testValue
-		} else {
-			n.identifier = n.identifier + "," + x.identifier
-			n.testValue = n.testValue + concat + x.testValue
-		}
+		nr := evaluationCriteria{identifier: x.identifier}
+		retmap[x.identifier] = nr
 	}
-	debugPrint("criteriaConcat(): concat result is \"%v\"\n", n.testValue)
-	ret = append(ret, n)
+	for _, x := range in {
+		retent := retmap[x.identifier]
+		if len(retmap[x.identifier].testValue) == 0 {
+			retent.testValue = x.testValue
+		} else {
+			retent.testValue = retent.testValue + concat + x.testValue
+		}
+		retmap[x.identifier] = retent
+	}
+	for _, x := range retmap {
+		debugPrint("criteriaConcat(): result \"%v\", \"%v\"\n", x.identifier, x.testValue)
+		ret = append(ret, x)
+	}
 	return ret
 }
