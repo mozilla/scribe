a7dc9097b7db1b6774130f1b5cc79a0b0ad9e411
Aaron Meihm <ameihm@mozilla.com>
2017-07-19 16:44:01 -0500

    make filename tests a go test

--- fileops_test.go
+++ fileops_test.go
@@ -217,3 +217,99 @@ var fileContentPolicyDoc = `
 func TestFileContentPolicy(t *testing.T) {
 	genericTestExec(t, fileContentPolicyDoc)
 }
+
+var fileNamePolicyDoc = `
+{
+	"variables": [
+	{ "key": "root", "value": "./test/filename" }
+	],
+
+	"objects": [
+	{
+		"object": "anyfile",
+		"filename": {
+			"path": "${root}",
+			"file": "(.*)"
+		}
+	},
+
+	{
+		"object": "nosuchfile",
+		"filename": {
+			"path": "${root}",
+			"file": ".*(nosuchfile).*"
+		}
+	},
+
+	{
+		"object": "fileversion",
+		"filename": {
+			"path": "${root}",
+			"file": "file-(\\S+).txt"
+		}
+	},
+
+	{
+		"object": "testfile0",
+		"filename": {
+			"path": "${root}",
+			"file": "^(testfile0)$"
+		}
+	}
+	],
+
+	"tests": [
+	{
+		"test": "filename0",
+		"expectedresult": true,
+		"object": "anyfile"
+	},
+
+	{
+		"test": "filename1",
+		"expectedresult": false,
+		"object": "nosuchfile"
+	},
+
+	{
+		"test": "filename2",
+		"expectedresult": true,
+		"object": "fileversion",
+		"evr": {
+			"operation": "<",
+			"value": "1.6.0"
+		}
+	},
+
+	{
+		"test": "filename3",
+		"expectedresult": false,
+		"object": "fileversion",
+		"evr": {
+			"operation": "<",
+			"value": "1.1.0"
+		}
+	},
+
+	{
+		"test": "filename4",
+		"expectedresult": true,
+		"object": "fileversion",
+		"regexp": {
+			"value": "1.2.3"
+		}
+	},
+
+	{
+		"test": "filename5",
+		"expectedresult": true,
+		"object": "testfile0"
+	}
+
+	]
+}
+`
+
+func TestFileNamePolicy(t *testing.T) {
+	genericTestExec(t, fileNamePolicyDoc)
+}
